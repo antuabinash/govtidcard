4db4f33547bbbc82081678562fa778d2cce87ccd
antuabinash <150573436+antuabinash@users.noreply.github.com>
2026-09-17 15:30:56 +0530

    Update cache name to v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sikhya-sathi-cache-v7';
+const CACHE_NAME = 'sikhya-sathi-cache-v8';
 
 // These are the files from your repo that will be cached for offline use
 const urlsToCache = [
